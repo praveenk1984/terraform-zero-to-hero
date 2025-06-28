54ad3aadd2a751b97d103dddfa44df4fcc5d1ea1
praveenk1984 <karnati.praveenchowdary@gmail.com>
2025-06-28 18:19:34 +0530

    Chnages made to main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,11 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "this" {
+  ami                     = "ami-05ffe3c48a9991133"
+  instance_type           = "t2.micro"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "praveen1984s3"
 }
